aab6dc18b99b97120a0c2f59a178b06bfd1fb302
waqasmustafa <mustafawaqas0@gmail.com>
2026-01-30 21:16:06 +0500

    fix: type mismatch in sent_notifications schema

--- shared/schema.ts
+++ shared/schema.ts
@@ -378,7 +378,7 @@ export const sentNotifications = pgTable("sent_notifications", {
     .references(() => notifications.id, { onDelete: "cascade" })
     .notNull(),
 
-  userId: uuid("user_id")
+  userId: varchar("user_id")
     .references(() => users.id, { onDelete: "cascade" })
     .notNull(),
 
