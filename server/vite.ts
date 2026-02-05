6506a4ffe9c8a18c0daa74b4ba13c815b062a7e6
Appnacollagerapo <rahultadvi8143@gmail.com>
2026-02-05 12:09:15 +0530

    fix production routing

--- server/vite.ts
+++ server/vite.ts
@@ -71,19 +71,35 @@ export async function setupVite(app: Express, server: Server) {
   });
 }
 
+// export function serveStatic(app: Express) {
+//   const distPath = path.resolve(__dirname, "public");
+
+//   if (!fs.existsSync(distPath)) {
+//     throw new Error(
+//       `Could not find the build directory: ${distPath}, make sure to build the client first`,
+//     );
+//   }
+
+//   app.use(express.static(distPath));
+
+//   // fall through to index.html if the file doesn't exist
+//   app.use("*", (_req, res) => {
+//     res.sendFile(path.resolve(distPath, "index.html"));
+//   });
+// }
+
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "..", "client", "dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPath}, make sure to build the client first`
     );
   }
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
